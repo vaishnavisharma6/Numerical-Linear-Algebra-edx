27dd06e83ee8d00450f41232e1c7e2840212a652
Vaishnavi Sharma <54501089+vaishnavisharma6@users.noreply.github.com>
2022-06-12 16:08:11 +0530

    Add files via upload

---
+++ svd.m
@@ -0,0 +1,21 @@
+IMG = imread('Frida.jpg');
+A = double(IMG(:,:,1));
+imshow(uint8(A))
+size(A)
+[U,Sigma,V] = svd(A);
+k = 1
+B = uint8(U(:,1:k) * Sigma(1:k,1:k) * V(:,1:k)');
+imshow(B)
+
+r = min(size(A))
+for k=1:r
+    imshow(uint8(U(:,1:k)*Sigma(1:k,1:k) * V(:,1:k)'));
+    input( strcat( num2str( k ), "    press return" ) );
+end
+figure
+r = min(size(A));
+plot([1:r],diag(Sigma),'X')
+loglog([1:r],diag(Sigma),'X')
+
+
+
